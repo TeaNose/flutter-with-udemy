a18440fc39500aaeaad3944bd99dc90bb9a6aa89
Agustinus Wikrama <agustinuswikrama@gmail.com>
2019-10-15 12:28:19 +0700

    Add conditional widget

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/agustinuswikrama/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/agustinuswikrama/Documents/Code/flutter-with-udemy"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/agustinuswikrama/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
